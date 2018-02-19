f80e130a248eecc3933cd3bca04a9e80865b3546
Vitalii_Chechel <vitalii_chechel@epam.com>
2018-02-19 13:20:28 +0200

    mockUp

--- src/main/java/top/veselushki/service/VeselushkiService.java
+++ src/main/java/top/veselushki/service/VeselushkiService.java
@@ -1,6 +1,7 @@
 package top.veselushki.service;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -9,31 +10,32 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
-
+@Slf4j
 @Service
 public class VeselushkiService {
     private static final int FIRST_PAGE = 1;
 
-    private final String veseluskiSite;
+    private final String veseluskiSitePage;
     private final int countPage;
 
-    public VeselushkiService(@Value("${site.veselushki}") String veseluskiSite,
-                             @Value("${count.page}") int countPage) {
-        this.veseluskiSite = veseluskiSite;
+    public VeselushkiService(@Value("${site.veselushki.page}") String veseluskiSitePage,
+                             @Value("${page.count}") int countPage) {
+        this.veseluskiSitePage = veseluskiSitePage;
         this.countPage = countPage;
     }
 
     public List<String> getLastTopicsName() throws IOException {
         List<String> lastTopicsName = new ArrayList<>();
         for (int i = FIRST_PAGE; i <= countPage; i++) {
-            String page = "page/" + i;
-            Elements links = Jsoup.connect(veseluskiSite + page).get().body()
-                    .select("article a");
+            Element body = Jsoup.connect(veseluskiSitePage + i).get().body();
+            Elements links = getArticleLinks(body);
             List<String> topicsName = links.stream()
                     .map(element -> element.attr("href"))
-                    .filter(url -> url.startsWith(veseluskiSite))
+                    .filter(url -> url.startsWith(veseluskiSitePage))
                     .map(this::getPageName)
                     .collect(toList());
             lastTopicsName.addAll(topicsName);
@@ -41,7 +43,11 @@ public class VeselushkiService {
         return lastTopicsName;
     }
 
+    private Elements getArticleLinks(Element body) {
+        return body.select("article a");
+    }
+
     private String getPageName(String pageUrl) {
-        return pageUrl.replace(veseluskiSite, EMPTY).replace("/", EMPTY);
+        return pageUrl.replace(veseluskiSitePage, EMPTY).replace("/", EMPTY);
     }
 }
--- src/test/java/top/veselushki/service/VeselushkiServiceIT.java
+++ src/test/java/top/veselushki/service/VeselushkiServiceIT.java
@@ -1,25 +1,38 @@
 package top.veselushki.service;
 
-import mockit.MockUp;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
 
 import java.io.IOException;
 
+import mockit.MockUp;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class VeselushkiServiceIT {
 
-    private VeselushkiService sut = mock(VeselushkiService.class);
+    private VeselushkiService sut = new VeselushkiService("Test",5);
+    private static Connection connection = mock(Connection.class);
 
     @Test
     public void shouldReturnLastTopicsName() throws IOException {
         //GIVEN
-        new MockUp<Jsoup>() {
-        Jsoup.connect()
-        };
+        mockJsoupConnection();
+        when(connection.get()).thenReturn(new Document("/veselushkiFirstPage.html"));
         //WHEN
         sut.getLastTopicsName();
         //THEN
+    }
+
+    private static void mockJsoupConnection() {
+        new MockUp<Jsoup>() {
+            @mockit.Mock
+            public Connection connect(String url) {
+                return connection;
+            }
+        };
     }
 }
