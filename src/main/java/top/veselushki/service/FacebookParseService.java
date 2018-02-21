7a189ed45b19c1cdd555ab7a179fe480390dacd8
Vitalii_Chechel <vitalii_chechel@epam.com>
2018-02-21 19:34:20 +0200

    start impl facebook service parser

--- src/main/java/top/veselushki/service/FacebookParseService.java
+++ src/main/java/top/veselushki/service/FacebookParseService.java
@@ -1,17 +1,20 @@
 package top.veselushki.service;
 
 import com.google.common.net.UrlEscapers;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import top.veselushki.dto.FacebookPost;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-import static javafx.beans.binding.Bindings.select;
+import top.veselushki.dto.FacebookPost;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class FacebookParseService {
@@ -21,13 +24,40 @@ public class FacebookParseService {
         this.facebookUrl = facebookUrl;
     }
 
-    public FacebookPost getPublishedPosts(List<String> pageName) throws IOException {
-        Element page = Jsoup.connect(facebookUrl).get().body();
-        Elements all_A_Tags = page.select("a");
-        UrlEscapers.urlFragmentEscaper().escape("http://veselushki.top/mamyi-vnimanie-posle-kakogo-muzhskogo-vozrasta-opasno-imet-detey/");
+    public List<FacebookPost> getPublishedPosts(List<String> topicLinks) throws IOException {
+        List<FacebookPost> publishedPosts = new ArrayList<>();
+        List<String> escapedTopicLinks = escapeLinks(topicLinks);
+        Elements allLinksOnFacebookPage = getAllLinks();
+        for (String escapedTopicLink : escapedTopicLinks) {
+            Element elementWithLinkToSite = allLinksOnFacebookPage.stream()
+                    .filter(a -> a.attr("href").contains(escapedTopicLink))
+                    .filter(element -> element instanceof Element)
+                    .map(Element.class::cast)
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Element with link ti site wasn't found for page:" + escapedTopicLink));
 
-        return null;
+            String label = getLabel(elementWithLinkToSite);
+            publishedPosts.add(new FacebookPost(escapedTopicLink, label));
+        }
+
+        return publishedPosts;
     }
 
+    private List<String> escapeLinks(List<String> links) {
+        return links.stream()
+                .map(UrlEscapers.urlPathSegmentEscaper()::escape)
+                .collect(toList());
+    }
+
+    private Elements getAllLinks() throws IOException {
+        Element page = Jsoup.connect(facebookUrl).get().body();
+        return page.select("a");
+    }
+
+    private String getLabel(Element elementWithLinkToSite) {
+        return elementWithLinkToSite.parent().parent().parent().parent().parent().parent().parent().parent().parent()
+                .child(1)
+                .child(0).text();
+    }
 
 }
